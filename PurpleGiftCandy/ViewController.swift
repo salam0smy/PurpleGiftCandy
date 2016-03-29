a1560d1715eeb16b91694cdab263d2c87f8a3bb5
salam0smy <salam0smy@gmail.com>
2016-03-29 00:48:30 -0400

    Initial Commit

---
+++ PurpleGiftCandy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PurpleGiftCandy
+//
+//  Created by Salam alyahya on 2016-03-29.
+//  Copyright © 2016 Salam alyahya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
